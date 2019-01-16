b719175713e063f81c857ed953d8535e9bfbcd0f
richardtam.ny <richardtam.ny@gmail.com>
2019-01-16 20:55:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
-  board.each{|x|} if {x} == "X" || {x} == "0"
-    counter += 1
+  board.each{|x|} if {x} == "X" || {x} == "O"
+    counter = counter += 1
   end
   counter
-end  
+end
